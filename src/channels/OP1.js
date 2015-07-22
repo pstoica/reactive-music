8185b61451cfa57ce2c972a3a7e237a41e0afa14
Patrick Stoica <hipatark@gmail.com>
2015-07-22 03:54:56 -0400

    functional hotswapping when changing a pattern stream. time doesn't get reset, and your changes happen on the next cycle

--- src/channels/OP1.js
+++ src/channels/OP1.js
@@ -1,14 +1,17 @@
 import µ from 'utils';
-import a from 'streams/a';
+let a = require('streams/a');
 import b from 'streams/b';
 
+console.log('reevaluate');
+
 export default (scheduler) => {
+  const a$ = new Rx.Subject();
+  a(scheduler).subscribe(::a$.onNext);
+
   const $ = µ.requestChannel('DEBUGGER').subscribe(
     (m) => {
       Obs
-        .merge(
-          a(scheduler)
-        )
+        .merge(a$)
         .subscribe(
           ({ time, ...note }) => {
             m.onNext({ note, time });
@@ -17,6 +20,11 @@ export default (scheduler) => {
     },
     (e) => ::console.error
   );
+
+  module.hot.accept('streams/a', (a) => {
+    a = require('streams/a');
+    a(scheduler).subscribe(::a$.onNext);
+  });
 
   module.hot && module.hot.dispose(() => $.dispose());
 
